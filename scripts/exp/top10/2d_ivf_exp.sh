707d085265c2b28d7e4e3213bb171103349668a6
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-04-07 21:52:31 +0800

    revise 2d IVF scripts

--- scripts/exp/top10/2d_ivf_exp.sh
+++ scripts/exp/top10/2d_ivf_exp.sh
@@ -4,7 +4,9 @@ r_s=("1100 1200" "3100 3200" "5100 5200" "8100 8200" "9100 9200")
 nprobe_s=(10 20 30 40 50)
 
 for dataset in ${datasets[@]}; do
-  for r in "${r_s[@]}"; do
-    /home/chunxy/repos/Compass/build/Release/src/benchmarks/bench_compass_ivf --datacard ${dataset}_2_10000_float32 --l 100 200 --r ${r} --k 10 --nlist 1000 --nprobe ${nprobe_s[@]}
+  for nlist in ${nlist_s[@]}; do
+    for r in "${r_s[@]}"; do
+      /home/chunxy/repos/Compass/build/Release/src/benchmarks/bench_compass_ivf --datacard ${dataset}_2_10000_float32 --l 100 200 --r ${r} --k 10 --nlist ${nlist} --nprobe ${nprobe_s[@]}
+    done
   done
 done
