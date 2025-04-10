55d3138b1ef6b12b65d47252aca7598733e42cc5
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-04-10 21:30:07 +0800

    tidy up 2d experiment scripts

--- scripts/exp/top10/2d_graph_exp.sh
+++ scripts/exp/top10/2d_graph_exp.sh
@@ -1,10 +1,13 @@
 datasets=(sift gist crawl glove100 video audio)
+M_s=(16 32)
 r_s=("1100 1200" "3100 3200" "5100 5200" "8100 8200" "9100 9200")
-efs_s=(10 20 60 100 200)
-nrel_s=(500 1000)
+efs_s=(100 200 300 400 500 1000)
+nrel_s=(100 200)
 
 for dataset in ${datasets[@]}; do
-  for r in "${r_s[@]}"; do
-    /home/chunxy/repos/Compass/build/Release/src/benchmarks/bench_compass_graph --datacard ${dataset}_2_10000_float32 --l 100 200 --r ${r} --k 10 --M 16 --efc 200 --efs ${efs_s[@]} --nrel ${nrel_s[@]}
+  for M in ${M_s[@]}; do
+    for r in "${r_s[@]}"; do
+      /home/chunxy/repos/Compass/build/Release/src/benchmarks/bench_compass_graph --datacard ${dataset}_2_10000_float32 --l 100 200 --r ${r} --k 10 --M ${M} --efc 200 --efs ${efs_s[@]} --nrel ${nrel_s[@]}
+    done
   done
 done
