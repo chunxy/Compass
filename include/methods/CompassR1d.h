0ae53434fae5fac121d07a269c787f5ef21b128e
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-04-21 17:01:37 +0800

    change the logic of CompassR1d

--- include/methods/CompassR1d.h
+++ include/methods/CompassR1d.h
@@ -204,7 +204,7 @@ class CompassR1d {
   ) {
     auto efs_ = std::max(k, efs);
     hnsw_.setEf(efs_);
-    int nprobe = 100;
+    int nprobe = ivf_->nlist / 10;
     this->ivf_->quantizer->search(nq, (float *)query, nprobe, distances, ranked_clusters);
 
     vector<vector<pair<dist_t, labeltype>>> results(nq, vector<pair<dist_t, labeltype>>(k));
@@ -215,50 +215,51 @@ class CompassR1d {
     for (int q = 0; q < nq; q++) {
       priority_queue<pair<float, int64_t>> top_candidates;
       priority_queue<pair<float, int64_t>> candidate_set;
+      priority_queue<pair<float, int64_t>> recycle_set;
 
       vector<bool> visited(hnsw_.cur_element_count, false);
 
       metrics[q].nround = 0;
       metrics[q].ncomp = 0;
 
-      {
-        tableint currObj = hnsw_.enterpoint_node_;
-        dist_t currDist = hnsw_.fstdistfunc_(
-            (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(hnsw_.enterpoint_node_), hnsw_.dist_func_param_
-        );
+      // {
+      //   tableint currObj = hnsw_.enterpoint_node_;
+      //   dist_t currDist = hnsw_.fstdistfunc_(
+      //       (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(hnsw_.enterpoint_node_), hnsw_.dist_func_param_
+      //   );
 
-        for (int level = hnsw_.maxlevel_; level > 0; level--) {
-          bool changed = true;
-          while (changed) {
-            changed = false;
-            unsigned int *data;
+      //   for (int level = hnsw_.maxlevel_; level > 0; level--) {
+      //     bool changed = true;
+      //     while (changed) {
+      //       changed = false;
+      //       unsigned int *data;
 
-            data = (unsigned int *)hnsw_.get_linklist(currObj, level);
-            int size = hnsw_.getListCount(data);
-            metrics[q].ncomp += size;
+      //       data = (unsigned int *)hnsw_.get_linklist(currObj, level);
+      //       int size = hnsw_.getListCount(data);
+      //       metrics[q].ncomp += size;
 
-            tableint *datal = (tableint *)(data + 1);
-            for (int i = 0; i < size; i++) {
-              tableint cand = datal[i];
+      //       tableint *datal = (tableint *)(data + 1);
+      //       for (int i = 0; i < size; i++) {
+      //         tableint cand = datal[i];
 
-              if (cand < 0 || cand > hnsw_.max_elements_) throw std::runtime_error("cand error");
-              dist_t d = hnsw_.fstdistfunc_(
-                  (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(cand), hnsw_.dist_func_param_
-              );
+      //         if (cand < 0 || cand > hnsw_.max_elements_) throw std::runtime_error("cand error");
+      //         dist_t d = hnsw_.fstdistfunc_(
+      //             (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(cand), hnsw_.dist_func_param_
+      //         );
 
-              if (d < currDist) {
-                currDist = d;
-                currObj = cand;
-                changed = true;
-              }
-            }
-          }
-        }
-        visited[currObj] = true;
-        candidate_set.emplace(-currDist, currObj);
-        metrics[q].is_graph_ppsl[currObj] = true;
-        if (pred(currObj)) top_candidates.emplace(currDist, currObj);
-      }
+      //         if (d < currDist) {
+      //           currDist = d;
+      //           currObj = cand;
+      //           changed = true;
+      //         }
+      //       }
+      //     }
+      //   }
+      //   visited[currObj] = true;
+      //   candidate_set.emplace(-currDist, currObj);
+      //   metrics[q].is_graph_ppsl[currObj] = true;
+      //   if (pred(currObj)) top_candidates.emplace(currDist, currObj);
+      // }
 
       int curr_ci = q * nprobe;
       auto itr_beg = btrees_[ranked_clusters[curr_ci]].lower_bound(l_bound);
@@ -291,14 +292,16 @@ class CompassR1d {
             auto vect = hnsw_.getDataByInternalId(tableid);
             auto dist = hnsw_.fstdistfunc_((float *)query + q * ivf_->d, vect, hnsw_.dist_func_param_);
             metrics[q].ncomp++;
+            metrics[q].is_ivf_ppsl[tableid] = true;
             crel++;
 
             auto upper_bound = top_candidates.empty() ? std::numeric_limits<dist_t>::max() : top_candidates.top().first;
-            if (top_candidates.size() < efs || dist < upper_bound) {
-              candidate_set.emplace(-dist, tableid);
+            candidate_set.emplace(-dist, tableid);
+            if (dist < upper_bound) {
               top_candidates.emplace(dist, tableid);
-              metrics[q].is_ivf_ppsl[tableid] = true;
               if (top_candidates.size() > efs_) top_candidates.pop();
+            } else {
+              recycle_set.emplace(-dist, tableid);
             }
           }
           metrics[q].nround++;
@@ -323,6 +326,20 @@ class CompassR1d {
       }
 
       metrics[q].ncluster = curr_ci - q * nprobe;
+      int nrecycled = 0;
+      while (top_candidates.size() > k) top_candidates.pop();
+      while (!recycle_set.empty()) {
+        auto top = recycle_set.top();
+        if (top_candidates.size() >= k && -top.first > top_candidates.top().first)
+          break;
+        else {
+          top_candidates.emplace(-top.first, top.second);
+          top_candidates.pop();
+          nrecycled++;
+        }
+        recycle_set.pop();
+      }
+      metrics[q].nrecycled = nrecycled;
       while (top_candidates.size() > k) top_candidates.pop();
       size_t sz = top_candidates.size();
       while (!top_candidates.empty()) {
@@ -334,162 +351,162 @@ class CompassR1d {
     return results;
   }
 
-//   vector<vector<pair<float, hnswlib::labeltype>>> SearchKnnV3(
-//       const void *query,
-//       const int nq,
-//       const int k,
-//       const attr_t &l_bound,
-//       const attr_t &u_bound,
-//       const int efs,
-//       const int nrel,
-//       const int nthread,
-//       vector<Metric> &metrics
-//   ) {
-//     auto efs_ = std::max(k, efs);
-//     hnsw_.setEf(efs_);
-//     int nprobe = ivf_->nlist;
-//     faiss::idx_t *ranked_clusters;
-//     // this->ivf_->quantizer->search(nq, (float *)query, nprobe, distances, ranked_clusters);
+  //   vector<vector<pair<float, hnswlib::labeltype>>> SearchKnnV3(
+  //       const void *query,
+  //       const int nq,
+  //       const int k,
+  //       const attr_t &l_bound,
+  //       const attr_t &u_bound,
+  //       const int efs,
+  //       const int nrel,
+  //       const int nthread,
+  //       vector<Metric> &metrics
+  //   ) {
+  //     auto efs_ = std::max(k, efs);
+  //     hnsw_.setEf(efs_);
+  //     int nprobe = ivf_->nlist;
+  //     faiss::idx_t *ranked_clusters;
+  //     // this->ivf_->quantizer->search(nq, (float *)query, nprobe, distances, ranked_clusters);
 
-//     vector<vector<pair<dist_t, labeltype>>> results(nq, vector<pair<dist_t, labeltype>>(k));
+  //     vector<vector<pair<dist_t, labeltype>>> results(nq, vector<pair<dist_t, labeltype>>(k));
 
-//     // #pragma omp parallel for num_threads(nthread) schedule(static)
-//     for (int q = 0; q < nq; q++) {
-//       priority_queue<pair<float, int64_t>> top_candidates;
-//       priority_queue<pair<float, int64_t>> candidate_set;
+  //     // #pragma omp parallel for num_threads(nthread) schedule(static)
+  //     for (int q = 0; q < nq; q++) {
+  //       priority_queue<pair<float, int64_t>> top_candidates;
+  //       priority_queue<pair<float, int64_t>> candidate_set;
 
-//       vector<bool> visited(hnsw_.cur_element_count, false);
+  //       vector<bool> visited(hnsw_.cur_element_count, false);
 
-//       RangeQuery<float> pred(l_bound, u_bound, &attrs_);
-//       metrics[q].nround = 0;
-//       metrics[q].ncomp = 0;
+  //       RangeQuery<float> pred(l_bound, u_bound, &attrs_);
+  //       metrics[q].nround = 0;
+  //       metrics[q].ncomp = 0;
 
-//       {
-//         tableint currObj = hnsw_.enterpoint_node_;
-//         dist_t currDist = hnsw_.fstdistfunc_(
-//             (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(hnsw_.enterpoint_node_), hnsw_.dist_func_param_
-//         );
+  //       {
+  //         tableint currObj = hnsw_.enterpoint_node_;
+  //         dist_t currDist = hnsw_.fstdistfunc_(
+  //             (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(hnsw_.enterpoint_node_), hnsw_.dist_func_param_
+  //         );
 
-//         for (int level = hnsw_.maxlevel_; level > 0; level--) {
-//           bool changed = true;
-//           while (changed) {
-//             changed = false;
-//             unsigned int *data;
+  //         for (int level = hnsw_.maxlevel_; level > 0; level--) {
+  //           bool changed = true;
+  //           while (changed) {
+  //             changed = false;
+  //             unsigned int *data;
 
-//             data = (unsigned int *)hnsw_.get_linklist(currObj, level);
-//             int size = hnsw_.getListCount(data);
-//             metrics[q].ncomp += size;
+  //             data = (unsigned int *)hnsw_.get_linklist(currObj, level);
+  //             int size = hnsw_.getListCount(data);
+  //             metrics[q].ncomp += size;
 
-//             tableint *datal = (tableint *)(data + 1);
-//             for (int i = 0; i < size; i++) {
-//               tableint cand = datal[i];
+  //             tableint *datal = (tableint *)(data + 1);
+  //             for (int i = 0; i < size; i++) {
+  //               tableint cand = datal[i];
 
-//               if (cand < 0 || cand > hnsw_.max_elements_) throw std::runtime_error("cand error");
-//               dist_t d = hnsw_.fstdistfunc_(
-//                   (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(cand), hnsw_.dist_func_param_
-//               );
+  //               if (cand < 0 || cand > hnsw_.max_elements_) throw std::runtime_error("cand error");
+  //               dist_t d = hnsw_.fstdistfunc_(
+  //                   (float *)query + q * ivf_->d, hnsw_.getDataByInternalId(cand), hnsw_.dist_func_param_
+  //               );
 
-//               if (d < currDist) {
-//                 currDist = d;
-//                 currObj = cand;
-//                 changed = true;
-//               }
-//             }
-//           }
-//         }
-//         ranked_clusters = ranked_clusters_ + currObj * nprobe;
-//         visited[currObj] = true;
-//         candidate_set.emplace(-currDist, currObj);
-//         top_candidates.emplace(currDist, currObj);
-//       }
+  //               if (d < currDist) {
+  //                 currDist = d;
+  //                 currObj = cand;
+  //                 changed = true;
+  //               }
+  //             }
+  //           }
+  //         }
+  //         ranked_clusters = ranked_clusters_ + currObj * nprobe;
+  //         visited[currObj] = true;
+  //         candidate_set.emplace(-currDist, currObj);
+  //         top_candidates.emplace(currDist, currObj);
+  //       }
 
-//       int curr_ci = 0;
-//       auto itr_beg = btrees_[ranked_clusters[0]].lower_bound(l_bound);
-//       auto itr_end = btrees_[ranked_clusters[0]].upper_bound(u_bound);
-//       //       while (itr_beg != itr_end) {
-//       //         tableint id = (*itr_beg).second;
-//       //         itr_beg++;
-//       //         visited[id] = true;
-//       // #ifdef USE_SSE
-//       //         _mm_prefetch(hnsw_.getDataByInternalId((*itr_beg).second), _MM_HINT_T0);
-//       // #endif
-//       //         auto vect = hnsw_.getDataByInternalId(id);
-//       //         auto dist = hnsw_.fstdistfunc_((float *)query + q * ivf_->d, vect, hnsw_.dist_func_param_);
-//       //         candidate_set.emplace(-dist, id);
-//       //         top_candidates.emplace(dist, id);
-//       //       }
-//       //       curr_ci = 1;
+  //       int curr_ci = 0;
+  //       auto itr_beg = btrees_[ranked_clusters[0]].lower_bound(l_bound);
+  //       auto itr_end = btrees_[ranked_clusters[0]].upper_bound(u_bound);
+  //       //       while (itr_beg != itr_end) {
+  //       //         tableint id = (*itr_beg).second;
+  //       //         itr_beg++;
+  //       //         visited[id] = true;
+  //       // #ifdef USE_SSE
+  //       //         _mm_prefetch(hnsw_.getDataByInternalId((*itr_beg).second), _MM_HINT_T0);
+  //       // #endif
+  //       //         auto vect = hnsw_.getDataByInternalId(id);
+  //       //         auto dist = hnsw_.fstdistfunc_((float *)query + q * ivf_->d, vect, hnsw_.dist_func_param_);
+  //       //         candidate_set.emplace(-dist, id);
+  //       //         top_candidates.emplace(dist, id);
+  //       //       }
+  //       //       curr_ci = 1;
 
-//       int cnt = 0;
-//       while (true) {
-//         int crel = 0;
-//         // if (candidate_set.empty() || distances[curr_ci] < -candidate_set.top().first) {
-//         if (candidate_set.empty() ||
-//             (!top_candidates.empty() && -candidate_set.top().first > top_candidates.top().first)) {
-//           while (crel < nrel) {
-//             if (itr_beg == itr_end) {
-//               if (++curr_ci == nprobe)
-//                 break;
-//               else {
-//                 itr_beg = btrees_[ranked_clusters[curr_ci]].lower_bound(l_bound);
-//                 itr_end = btrees_[ranked_clusters[curr_ci]].upper_bound(u_bound);
-//                 continue;
-//               }
-//             }
+  //       int cnt = 0;
+  //       while (true) {
+  //         int crel = 0;
+  //         // if (candidate_set.empty() || distances[curr_ci] < -candidate_set.top().first) {
+  //         if (candidate_set.empty() ||
+  //             (!top_candidates.empty() && -candidate_set.top().first > top_candidates.top().first)) {
+  //           while (crel < nrel) {
+  //             if (itr_beg == itr_end) {
+  //               if (++curr_ci == nprobe)
+  //                 break;
+  //               else {
+  //                 itr_beg = btrees_[ranked_clusters[curr_ci]].lower_bound(l_bound);
+  //                 itr_end = btrees_[ranked_clusters[curr_ci]].upper_bound(u_bound);
+  //                 continue;
+  //               }
+  //             }
 
-//             auto tableid = (*itr_beg).second;
-//             itr_beg++;
-// #ifdef USE_SSE
-//             _mm_prefetch(hnsw_.getDataByInternalId((*itr_beg).second), _MM_HINT_T0);
-// #endif
-//             if (visited[tableid]) continue;
-//             visited[tableid] = true;
+  //             auto tableid = (*itr_beg).second;
+  //             itr_beg++;
+  // #ifdef USE_SSE
+  //             _mm_prefetch(hnsw_.getDataByInternalId((*itr_beg).second), _MM_HINT_T0);
+  // #endif
+  //             if (visited[tableid]) continue;
+  //             visited[tableid] = true;
 
-//             auto vect = hnsw_.getDataByInternalId(tableid);
-//             auto dist = hnsw_.fstdistfunc_((float *)query + q * ivf_->d, vect, hnsw_.dist_func_param_);
-//             metrics[q].ncomp++;
-//             crel++;
+  //             auto vect = hnsw_.getDataByInternalId(tableid);
+  //             auto dist = hnsw_.fstdistfunc_((float *)query + q * ivf_->d, vect, hnsw_.dist_func_param_);
+  //             metrics[q].ncomp++;
+  //             crel++;
 
-//             auto upper_bound = top_candidates.empty() ? std::numeric_limits<dist_t>::max() : top_candidates.top().first;
-//             if (top_candidates.size() < efs || dist < upper_bound) {
-//               candidate_set.emplace(-dist, tableid);
-//               top_candidates.emplace(dist, tableid);
-//               metrics[q].is_ivf_ppsl[tableid] = true;
-//               if (top_candidates.size() > efs_) top_candidates.pop();
-//             }
-//           }
-//           metrics[q].nround++;
-//         }
+  //             auto upper_bound = top_candidates.empty() ? std::numeric_limits<dist_t>::max() :
+  //             top_candidates.top().first; if (top_candidates.size() < efs || dist < upper_bound) {
+  //               candidate_set.emplace(-dist, tableid);
+  //               top_candidates.emplace(dist, tableid);
+  //               metrics[q].is_ivf_ppsl[tableid] = true;
+  //               if (top_candidates.size() > efs_) top_candidates.pop();
+  //             }
+  //           }
+  //           metrics[q].nround++;
+  //         }
 
-//         hnsw_.ReentrantSearchKnn(
-//             (float *)query + q * ivf_->d,
-//             k,
-//             -1,
-//             top_candidates,
-//             candidate_set,
-//             visited,
-//             &pred,
-//             std::ref(metrics[q].ncomp),
-//             std::ref(metrics[q].is_graph_ppsl)
-//         );
-//         // if ((top_candidates.size() >= efs_ && min_comp - metrics[q].ncomp < 0) ||
-//         //     curr_ci >= (q + 1) * nprobe) {
-//         if ((top_candidates.size() >= efs_) || curr_ci >= nprobe) {
-//           break;
-//         }
-//       }
+  //         hnsw_.ReentrantSearchKnn(
+  //             (float *)query + q * ivf_->d,
+  //             k,
+  //             -1,
+  //             top_candidates,
+  //             candidate_set,
+  //             visited,
+  //             &pred,
+  //             std::ref(metrics[q].ncomp),
+  //             std::ref(metrics[q].is_graph_ppsl)
+  //         );
+  //         // if ((top_candidates.size() >= efs_ && min_comp - metrics[q].ncomp < 0) ||
+  //         //     curr_ci >= (q + 1) * nprobe) {
+  //         if ((top_candidates.size() >= efs_) || curr_ci >= nprobe) {
+  //           break;
+  //         }
+  //       }
 
-//       while (top_candidates.size() > k) top_candidates.pop();
-//       size_t sz = top_candidates.size();
-//       // vector<std::pair<dist_t, labeltype>> result(sz);
-//       while (!top_candidates.empty()) {
-//         results[q][--sz] = top_candidates.top();
-//         top_candidates.pop();
-//       }
-//     }
+  //       while (top_candidates.size() > k) top_candidates.pop();
+  //       size_t sz = top_candidates.size();
+  //       // vector<std::pair<dist_t, labeltype>> result(sz);
+  //       while (!top_candidates.empty()) {
+  //         results[q][--sz] = top_candidates.top();
+  //         top_candidates.pop();
+  //       }
+  //     }
 
-//     return results;
-//   }
+  //     return results;
+  //   }
 
   vector<vector<pair<float, hnswlib::labeltype>>> SearchKnnV4(
       const void *query,
