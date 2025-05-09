58e072d95e1fb98d092d71381061b1efc35008a1
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-05-09 12:53:16 +0800

    add statistics for Proclus

---
+++ src/apps/top_k_in_cluster_hist_proclus.cpp
@@ -0,0 +1,84 @@
+#include <fmt/core.h>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <string>
+#include "config.h"
+#include "faiss/MetricType.h"
+#include "methods/Proclus.h"
+#include "utils/card.h"
+#include "utils/funcs.h"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+int main(int argc, char **argv) {
+  po::options_description configs;
+  int k;
+  float l, r;
+  string datacard;
+  int nlist;
+  int dproclus;
+  configs.add_options()("k", po::value<decltype(k)>(&k)->required());
+  configs.add_options()("l", po::value<decltype(l)>(&l)->required());
+  configs.add_options()("r", po::value<decltype(r)>(&r)->required());
+  configs.add_options()("nlist", po::value<decltype(nlist)>(&nlist)->required());
+  configs.add_options()("dproclus", po::value<decltype(dproclus)>(&dproclus)->required());
+  configs.add_options()("datacard", po::value<decltype(datacard)>(&datacard)->required());
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, configs), vm);
+  po::notify(vm);
+
+  extern std::map<std::string, DataCard> name_to_card;
+  DataCard c = name_to_card[datacard];
+  int d = c.dim;
+
+  float *xb, *xq;
+  uint32_t *gt;
+  vector<vector<float>> _attrs;
+  load_hybrid_data(c, xb, xq, gt, _attrs);
+  vector<vector<labeltype>> hybrid_topks;
+  load_hybrid_query_gt(c, {l}, {r}, k, hybrid_topks);
+
+  fs::path ckp_root(CKPS);
+  std::string medoids_ckp = fmt::format("{}-{}.medoids", nlist, dproclus);
+  std::string subspace_ckp = fmt::format("{}-{}.subspaces", nlist, dproclus);
+  Proclus proclus(nlist, d);
+  proclus.read_subspaces((ckp_root / "Proclus" / c.name / subspace_ckp).string());
+  std::ifstream in(ckp_root / "Proclus" / c.name / medoids_ckp);
+  in.read((char *)proclus.medoids, nlist * d * sizeof(float));
+
+  float* distances = new float[c.n_queries * nlist];
+  auto q_ranked_clusters = new faiss::idx_t[c.n_queries * nlist];
+  proclus.search(c.n_queries, xq, q_ranked_clusters, nlist);
+
+  auto b_ranked_clusters = new faiss::idx_t[c.n_base];
+  proclus.search(c.n_base, xb, b_ranked_clusters, 1);
+
+  int32_t *hist = new int32_t[nlist];
+  memset(hist, 0, sizeof(int32_t) * nlist); // Corrected sizeof(int) to sizeof(int32_t)
+  for (int i = 0; i < c.n_queries; i++) {
+    for (int j = 0; j < k; j++) {
+      for (int r = 0; r < nlist; r++) {
+        // int lo = i * nlist, hi = (i+1) * nlist;
+        if (b_ranked_clusters[hybrid_topks[i][j]] == q_ranked_clusters[i * nlist + r]) {
+          hist[r]++;
+          break;
+        }
+      }
+    }
+  }
+
+  string hist_file = fmt::format("proclus_top_{}_in_cluster_hist_{}_{}_{}_{}.bin", k, c.name, nlist, dproclus, l, r);
+  fs::path stat_root(STATS);
+  fs::path hist_path = stat_root / hist_file;
+  std::ofstream out(hist_path.c_str());
+  for (int i = 0; i < nlist; i++) {
+    out.write((char *)(hist + i), sizeof(int32_t));
+  }
+
+  return 0;
+}
---
+++ stat_proclus.py
@@ -0,0 +1,68 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
+
+dataset_config = {"sift": [(1000, 64)]}
+dataset_nb = {"sift": 1_000_000}
+
+def stat_top_k_in_cluster_by_selectivity():
+  STATS_DIR = "/home/chunxy/repos/Compass/stats"
+  stat_dir = Path(STATS_DIR)
+  stat_tmpl = "proclus_top_{}_in_cluster_hist_{}_{}_{}_{}_{}.bin"
+  k = 10
+  ranges = [(0, 10000), *[(100, r) for r in (200, 300, 600)], *[(100, r) for r in range(1100, 10000, 1000)]]
+
+  for i, d in enumerate(dataset_config.keys()):
+    for rg in ranges:
+      fig, axs = plt.subplots(1, len(dataset_config[d]) + 1, layout='constrained')
+      for config in dataset_config[d]:
+        nlist, dim = config
+        stat_path = stat_dir / stat_tmpl.format(k, d, nlist, dim, *rg)
+        if not stat_path.exists(): continue
+        with open(stat_path, "rb") as hist_file:
+          data = list(int.from_bytes(hist_file.read(4), byteorder='little') for _ in range(nlist))
+          bins = list(range(0, len(data) + 1))
+          hist, _, _ = axs[i].hist(bins[:-1], bins, weights=data, density=True)
+          # axs[i].vlines(50, ymin=0, ymax=0.05, linestyles='dashed', c='r')
+          target = 0.9
+          j, cumu = 0, 0
+          while j < len(hist):
+            cumu += hist[j]
+            if cumu >= target:
+              break
+            j += 1
+          axs[i].annotate(f"{target:.1%}, {j}", xy=(j, 0), xytext=(j, 0.05), arrowprops=dict(facecolor='black', shrink=0.05))
+          axs[i].set_ylim(0, 0.5)
+          axs[i].set_title(f"{d.upper()}-{rg}-{nlist}-{dim}")
+      fig.set_size_inches(20, 5)
+      fig.savefig(f"figures_10/clustering/{d.upper()}-Proclus-{(rg[1] - rg[0]) / 10000:.1%}-Clustering-Quality.jpg", dpi=100)
+      plt.close()
+
+def stat_cluster_imbalance_factor():
+  STATS_DIR = "/home/chunxy/repos/Compass/checkpoints/Proclus"
+  stat_dir = Path(STATS_DIR)
+  for d, configs in dataset_config.items():
+    fig, axs = plt.subplots(1, len(configs), layout='constrained')
+    axs = axs.flat
+    for i, c in enumerate(configs):
+      nlist, dproclus = c
+      stat_tmpl = f"{nlist}-{dproclus}.ranking"
+      stat_path = stat_dir / d / stat_tmpl
+      if not stat_path.exists(): continue
+      with open(stat_path, "rb") as assignment_file:
+        assigned_clusters = list(int.from_bytes(assignment_file.read(8), byteorder='little') for _ in range(dataset_nb[d]))
+        counts = np.zeros(nlist)
+        for cluster in assigned_clusters:
+          counts[cluster] += 1
+        counts.sort()
+        counts = counts[::-1]
+        bins = list(range(0, nlist + 1))
+        axs[i].hist(bins[:-1], bins, weights=counts, density=True)
+        axs[i].set_ylim(0, 0.015)
+        axs[i].set_title(f"{d.upper()}-{nlist}-{dproclus}")
+    fig.set_size_inches(20, 5)
+    fig.savefig(f"figures_10/clustering/{d.upper()}-Proclus-Cluster-Imbalance.jpg", dpi=200)
+    plt.close(fig)
+
+# stat_cluster_imbalance_factor()
+stat_top_k_in_cluster_by_selectivity()
