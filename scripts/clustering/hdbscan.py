814c082b53ff68f24e18db9e2ed33cc2796f2e7f
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-05-21 15:39:11 +0800

    use sampling

--- scripts/clustering/hdbscan.py
+++ scripts/clustering/hdbscan.py
@@ -14,6 +14,7 @@ datasets = {
 
 parser = argparse.ArgumentParser(description="Clustering script")
 parser.add_argument("--name", type=str, required=True, help="Dataset name to process")
+parser.add_argument("--n_samples", type=int, required=True, help="Number of samples to use")
 args = parser.parse_args()
 
 if args.name not in datasets:
@@ -21,13 +22,15 @@ if args.name not in datasets:
 
 name = args.name
 d = datasets[name]
+n_samples = args.n_samples
 
 file = f"/home/chunxy/repos/Compass/data/{name}_base.float32"
 training_data = np.fromfile(file, dtype=np.float32).reshape((-1, d))
+sample_data = np.random.permutation(training_data)[:n_samples]
 
 # Perform HDBSCAN clustering
 hdbscan = HDBSCAN(min_cluster_size=100, store_centers="medoid")
-hdbscan.fit(training_data)
+hdbscan.fit(sample_data)
 centroids_hdbscan = hdbscan.centroids_.astype(np.float32)
 nlist = len(centroids_hdbscan)
 centroids_hdbscan.tofile(f"/home/chunxy/repos/Compass/data/hdbscan/{name}.{nlist}.hdbscan.medoids")
