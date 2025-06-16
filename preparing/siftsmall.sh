0017d2bc1a428d2166d4a7bdb165b6d480a06d74
ChunXiao Ye <chunxy@ssd34.dbg.private>
2025-06-16 17:29:01 +0800

    small fix

--- preparing/siftsmall.sh
+++ preparing/siftsmall.sh
@@ -5,15 +5,15 @@
 /home/chunxy/repos/Compass/build/Release/src/apps/generate-attributes --datacard siftsmall_1_1000_int32
 
 # Groundtruth preparation
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 110 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 120 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 150 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 200 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 300 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 600 --k 100
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 1000 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 110 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 120 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 150 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 200 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 300 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 600 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_1_1000_float32 --l 100 --r 1000 --k 100
 
-/home/chunxy/repos/Compass/build/src/Release/apps/compute-groundtruth --datacard siftsmall_2_1000_float32 --l 100 200 --r 600 700 --k 100
+/home/chunxy/repos/Compass/build/Release/src/apps/compute-groundtruth --datacard siftsmall_2_1000_float32 --l 100 200 --r 600 700 --k 100
 
 /home/chunxy/repos/Compass/build/Release/src/apps/compute_filter_groundtruth --datacard siftsmall_1_10_int32 --k 100
 /home/chunxy/repos/Compass/build/Release/src/apps/compute_filter_groundtruth --datacard siftsmall_1_1000_int32 --k 100
