c7d4148f700301294f59ac514ff1b226960eb206
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-06-17 14:39:57 +0800

    set arguments separately for datasets

--- scripts/compose.py
+++ scripts/compose.py
@@ -28,34 +28,35 @@ def compose():
         parts = [p.lower() for p in re.findall(r'[A-Z][a-z]*', m)]
         filename = f"{da}d-" + "-".join(parts[1:]) + f"-exp-{group}.sh"
         with open(EXP_ROOT / filename, "w") as f:
-          f.write(f'datasets=({" ".join(datasets)})\n')
-          for bp in M_PARAM[m]["build"]:
-            f.write(f'{bp}_s=({" ".join(map(str, dataset_args[datasets[0]].get(bp, compass_args[bp])))})\n')
-          for sp in M_PARAM[m]["search"]:
-            f.write(f'{sp}_s=({" ".join(map(str,compass_args[sp]))})\n')
+          for d in datasets:
+            f.write(f'dataset={d}\n')
+            for bp in M_PARAM[m]["build"]:
+              f.write(f'{bp}_s=({" ".join(map(str, dataset_args[d].get(bp, compass_args[bp])))})\n')
+            for sp in M_PARAM[m]["search"]:
+              f.write(f'{sp}_s=({" ".join(map(str,compass_args[sp]))})\n')
 
-          build_string = " ".join(map(lambda x: f"--{x} ${{{x}}}", M_PARAM[m]["build"]))
-          search_string = " ".join(map(lambda x: f"--{x} ${{{x}_s[@]}}", M_PARAM[m]["search"]))
-          intervals = M_DA_RUN[m][da]
-          inner_tmpl = \
+            build_string = " ".join(map(lambda x: f"--{x} ${{{x}}}", M_PARAM[m]["build"]))
+            search_string = " ".join(map(lambda x: f"--{x} ${{{x}_s[@]}}", M_PARAM[m]["search"]))
+            intervals = M_DA_RUN[m][da]
+            inner_tmpl = \
 '''/home/chunxy/repos/Compass/build/Release/src/benchmarks/bench-compass-{} \
 --datacard ${{dataset}}_{}_10000_float32 \
 --l {} --r {} --k 10 {} {}'''
-          inner = '\n'.join([
-            inner_tmpl.format(
-              ("1d-" if da == 1 else "") + "-".join(parts[1:]),
-              da,
-              " ".join(map(str, itvl[0])),
-              " ".join(map(str, itvl[1])),
-              build_string,
-              search_string,
-            ) for itvl in intervals
-          ])
+            inner = '\n'.join([
+              inner_tmpl.format(
+                ("1d-" if da == 1 else "") + "-".join(parts[1:]),
+                da,
+                " ".join(map(str, itvl[0])),
+                " ".join(map(str, itvl[1])),
+                build_string,
+                search_string,
+              ) for itvl in intervals
+            ])
 
-          for bp in M_PARAM[m]["build"][::-1]:
-            inner = __enclose_for(bp, f"{bp}_s", inner)
-          inner = __enclose_for("dataset", "datasets", inner)
-          f.write(inner)
+            for bp in M_PARAM[m]["build"][::-1]:
+              inner = __enclose_for(bp, f"{bp}_s", inner)
+            f.write(inner)
+            f.write('\n')
 
 
 if __name__ == "__main__":
