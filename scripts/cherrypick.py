cadd174a0f717f916ce794fa55d04f212aac5dd6
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-06-24 21:14:03 +0800

    add figures with varying nlist

--- scripts/cherrypick.py
+++ scripts/cherrypick.py
@@ -28,18 +28,6 @@ for d in DATASETS:
   d_m_b_M32[d]["iRangeGraph"] = ["M_32_efc_200"]
   d_m_b_M32[d]["SeRF"] = ["M_32_efc_200_efmax_500"]
 
-d_m_b_M32_64 = {d: {} for d in DATASETS}
-for d in ("sift", "audio"):
-  for m in COMPASS_METHODS:
-    d_m_b_M32_64[d][m] = ["M_16_efc_200_nlist_10000", "M_32_efc_200_nlist_10000"]
-  for m in COMPASSX_METHODS:
-    d_m_b_M32_64[d][m] = [f"M_{M}_efc_200_nlist_10000_dx_{dx}" for M in [16, 32] for dx in dataset_args[d]["dx"]]
-for d in ("gist", "video", "crawl", "glove100"):
-  for m in COMPASS_METHODS:
-    d_m_b_M32_64[d][m] = ["M_16_efc_200_nlist_20000", "M_32_efc_200_nlist_20000"]
-  for m in COMPASSX_METHODS:
-    d_m_b_M32_64[d][m] = [f"M_{M}_efc_200_nlist_20000_dx_{dx}" for M in [16, 32] for dx in dataset_args[d]["dx"]]
-
 nrel_100 = [100]
 nrel_100_200 = [100, 200]
 clus_methods = ["CompassK", "CompassBikmeans", "CompassPca"]
@@ -149,6 +137,14 @@ def compare_varying_nrel():
 
 
 def compare_varying_M():
+  d_m_b_M32_64 = {d: {} for d in DATASETS}
+  for d in ("sift", "audio"):
+    for m in clus_search_methods:
+      d_m_b_M32_64[d][m] = ["M_16_efc_200_nlist_10000", "M_32_efc_200_nlist_10000"]
+  for d in ("gist", "video", "crawl", "glove100"):
+    for m in clus_search_methods:
+      d_m_b_M32_64[d][m] = ["M_16_efc_200_nlist_20000", "M_32_efc_200_nlist_20000"]
+
   for da in DA_S:
     draw_qps_comp_wrt_recall_by_selectivity(
       da=da,
@@ -161,9 +157,31 @@ def compare_varying_M():
     )
 
 
+def compare_varying_nlist():
+  d_m_b_M32_smallnlist = {d: {} for d in DATASETS}
+  for d in ("sift", "audio"):
+    for m in clus_search_methods:
+      d_m_b_M32_smallnlist[d][m] = [f"M_16_efc_200_nlist_{nlist}" for nlist in [5000, 10000]]
+  for d in ("gist", "video", "crawl", "glove100"):
+    for m in clus_search_methods:
+      d_m_b_M32_smallnlist[d][m] = [f"M_16_efc_200_nlist_{nlist}" for nlist in [10000, 20000]]
+
+  for da in DA_S:
+    draw_qps_comp_wrt_recall_by_dataset_selectivity(
+      da=da,
+      datasets=DATASETS,
+      methods=clus_search_methods,
+      anno="nlist",
+      d_m_b=d_m_b_M32_smallnlist,
+      nrel_s=nrel_100,
+      prefix=f"cherrypick{da}d-10",
+    )
+
+
 if __name__ == "__main__":
   # compare_clustering_methods()
   # compare_cluster_search_methods()
   # compare_with_sotas()
-  compare_varying_nrel()
-  compare_varying_M()
+  # compare_varying_nrel()
+  # compare_varying_M()
+  compare_varying_nlist()
