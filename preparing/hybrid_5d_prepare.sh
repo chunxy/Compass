fcb3b2e0cef754f8eed35d3cd782a59edbba9416
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-08-08 14:18:18 +0800

    add 5d and 6d preparation scripts

---
+++ preparing/hybrid_5d_prepare.sh
@@ -0,0 +1,6 @@
+datasets=(sift gist crawl glove100 video audio)
+
+# Generate hybrid attributes
+for dataset in ${datasets[@]}; do
+  /home/chunxy/repos/Compass/build/Release/src/apps/generate-attributes --datacard ${dataset}_5_10000_float32
+done
