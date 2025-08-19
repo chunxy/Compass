8f8813fb43068aa745562382820f14600c4055d7
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-08-19 15:11:30 +0800

    change the result saving directory

--- src/benchmarks/bench-compass-1d-ivf.cpp
+++ src/benchmarks/bench-compass-1d-ivf.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
   int nq = c.n_queries;      // number of queries
   int ng = c.n_groundtruth;  // number of computed groundtruth entries
 
-  std::string method = "CompassIvf1d";
+  std::string method = "CompassIvf";
   std::string workload = fmt::format(HYBRID_WORKLOAD_TMPL, c.name, c.attr_range, args.l_bound, args.u_bound, args.k);
   std::string build_param = fmt::format("nlist_{}", args.nlist);
 
