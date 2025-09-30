07fe0d59780665aa704bb4c58f2f247270fc127f
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-09-30 12:45:32 +0800

    place different M arguments onto different machines

--- scripts/compose.py
+++ scripts/compose.py
@@ -27,39 +27,45 @@ def compose():
     for m in COMPASS_METHODS + BASE_METHODS:
       if da not in M_DA_RUN[m]:
         continue
+      idx = 0
       for group, datasets in M_GROUP_DATASET[m].items():
         parts = [p.lower() for p in re.findall(r'[A-Z][a-z]*', m)]
-        filename = f"{da}d-" + "-".join(parts[1:]) + f"-exp-{group}.sh"
-        with open(EXP_ROOT / filename, "w") as f:
-          for d in datasets:
-            f.write(f'dataset={d}\n')
-            for bp in M_PARAM[m]["build"]:
-              f.write(f'{bp}_s=({" ".join(map(str, D_ARGS[d].get(bp, M_ARGS[m][bp])))})\n')
-            for sp in M_PARAM[m]["search"]:
-              f.write(f'{sp}_s=({" ".join(map(str, D_ARGS[d].get(sp, M_ARGS[m][sp])))})\n')
+        for d in datasets:
+          for M in D_ARGS[d].get("M", M_ARGS[m].get("M", [1])):
+            idx += 1
+            filename = f"{da}d-" + "-".join(parts[1:]) + f"-exp-{idx}.sh"
+            with open(EXP_ROOT / filename, "w") as f:
+              f.write(f'dataset={d}\n')
+              for bp in M_PARAM[m]["build"]:
+                if bp == "M":
+                  f.write(f'{bp}_s=({M})\n')
+                else:
+                  f.write(f'{bp}_s=({" ".join(map(str, D_ARGS[d].get(bp, M_ARGS[m][bp])))})\n')
+              for sp in M_PARAM[m]["search"]:
+                f.write(f'{sp}_s=({" ".join(map(str, D_ARGS[d].get(sp, M_ARGS[m][sp])))})\n')
 
-            build_string = " ".join(map(lambda x: f"--{x} ${{{x}}}", M_PARAM[m]["build"]))
-            search_string = " ".join(map(lambda x: f"--{x} ${{{x}_s[@]}}", M_PARAM[m]["search"]))
-            intervals = M_DA_RUN[m][da]
-            inner_tmpl = \
+              build_string = " ".join(map(lambda x: f"--{x} ${{{x}}}", M_PARAM[m]["build"]))
+              search_string = " ".join(map(lambda x: f"--{x} ${{{x}_s[@]}}", M_PARAM[m]["search"]))
+              intervals = M_DA_RUN[m][da]
+              inner_tmpl = \
 '''/home/chunxy/repos/Compass/build/Release/src/benchmarks/bench-compass-{} \
 --datacard ${{dataset}}_{}_10000_float32 \
 --l {} --r {} --k 10 {} {}'''
-            inner = '\n'.join([
-              inner_tmpl.format(
-                ("1d-" if da == 1 else "") + "-".join(parts[1:]),
-                da,
-                " ".join(map(str, itvl[0])),
-                " ".join(map(str, itvl[1])),
-                build_string,
-                search_string,
-              ) for itvl in intervals
-            ])
+              inner = '\n'.join([
+                inner_tmpl.format(
+                  ("1d-" if da == 1 else "") + "-".join(parts[1:]),
+                  da,
+                  " ".join(map(str, itvl[0])),
+                  " ".join(map(str, itvl[1])),
+                  build_string,
+                  search_string,
+                ) for itvl in intervals
+              ])
 
-            for bp in M_PARAM[m]["build"][::-1]:
-              inner = __enclose_for(bp, f"{bp}_s", inner)
-            f.write(inner)
-            f.write('\n')
+              for bp in M_PARAM[m]["build"][::-1]:
+                inner = __enclose_for(bp, f"{bp}_s", inner)
+              f.write(inner)
+              f.write('\n')
 
 
 if __name__ == "__main__":
