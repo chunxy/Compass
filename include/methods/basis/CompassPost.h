73499de52431a0e4d06811315fce07375b65c615
ChunXiao Ye <chunxy@ssd32.dbg.private>
2025-10-07 10:54:02 +0800

    test CompassPost with bitset

--- include/methods/basis/CompassPost.h
+++ include/methods/basis/CompassPost.h
@@ -7,6 +7,7 @@
 #include "fc/btree.h"
 #include "hnswlib/hnswlib.h"
 #include "methods/basis/IterativeSearch.h"
+#include "roaring/roaring.hh"
 #include "utils/Pod.h"
 
 using std::array;
@@ -23,6 +24,7 @@ class CompassPost {
   faiss::Index *ivf_;
   // vector<btree::btree_map<attr_t, labeltype>> btrees_;
   vector<fc::BTreeMap<attr_t, pair<labeltype, array<attr_t, 4>>, 32>> btrees_;
+  vector<fc::BTreeMap<attr_t, labeltype>> mbtrees_;
   faiss::idx_t *base_cluster_rank_;
   // faiss::idx_t *query_cluster_rank_;
   // dist_t *distances_;
@@ -51,6 +53,7 @@ class CompassPost {
         graph_(n, d, new L2Space(d), M),
         cg_(nlist, d, new L2Space(d), M_cg),
         btrees_(nlist),
+        mbtrees_(nlist * da),
         base_cluster_rank_(new faiss::idx_t[n]) {
     cg_.SetSearchParam(batch_k, initial_efs, delta_efs);
     ivf_ = new faiss::IndexIVFFlat(new faiss::IndexFlatL2(d), d, nlist);
@@ -107,6 +110,7 @@ class CompassPost {
       array<attr_t, 4> arr{0, 0, 0, 0};
       for (int j = 0; j < this->da_; j++) {
         arr[j] = attrs[i * this->da_ + j];
+        mbtrees_[this->base_cluster_rank_[i] * this->da_ + j][attrs[i * this->da_ + j]] = labels[i];
       }
       btrees_[this->base_cluster_rank_[i]][attrs[i]] = std::make_pair(labels[i], arr);
     }
@@ -627,6 +631,229 @@ class CompassPost {
 #ifdef USE_SSE
             if (itr_beg != itr_end)
               _mm_prefetch(this->graph_.hnsw_->getDataByInternalId(itr_beg->second.first), _MM_HINT_T0);
+#endif
+            if (vl->mass[tableid] == vl->curV) {
+              continue;
+            }
+            vl->mass[tableid] = vl->curV;
+            auto vect = this->graph_.hnsw_->getDataByInternalId(tableid);
+            auto dist = this->graph_.hnsw_->fstdistfunc_(query_q, vect, this->graph_.hnsw_->dist_func_param_);
+            bm.qmetrics[q].ncomp++;
+            top_ivf.push(std::make_pair(-dist, tableid));
+            crel++;
+          }
+          int i = 0;
+          for (; i < k && !top_ivf.empty(); i++) {
+            auto top = top_ivf.top();
+            top_ivf.pop();
+            // TODO: consider bounding by the top of top_candidates
+            state.candidate_set_.emplace(top.first, top.second);
+            // state.result_set_.emplace(top.first, top.second);
+            top_candidates.emplace(-top.first, top.second);
+            state.top_candidates_.emplace(-top.first, top.second);
+            bm.qmetrics[q].is_ivf_ppsl[top.second] = true;
+            vl->mass[top.second] = vl->curV;
+            num_ivf_ppsl++;
+          }
+          graph_.hnsw_->setEf(graph_.hnsw_->ef_ + i);
+          state.sel_ = 1;  // restart graph
+          continue;
+#ifndef BENCH
+          auto ivf_stop = std::chrono::high_resolution_clock::system_clock::now();
+          auto ivf_time = std::chrono::duration_cast<std::chrono::nanoseconds>(ivf_stop - ivf_start).count();
+          bm.qmetrics[q].ivf_latency += ivf_time;
+#endif
+        }
+        // Believe in graph when the first-hop selectivity is not low.
+        // "state.sel_ >=" means we do not always rely on graph.
+        if (nround_graph == 0 || state.sel_ >= breaktie) {
+#ifndef BENCH
+          auto graph_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+          priority_queue<pair<dist_t, labeltype>> batch = graph_.NextBatchTwoHop(&state, &pred);
+          num_graph_ppsl += batch.size();
+          graph_last_round = batch.size();
+          while (!batch.empty()) {
+            auto [dist, label] = batch.top();
+            batch.pop();
+            // if (pred(label)) {
+            top_candidates.push(std::make_pair(-dist, label));
+            bm.qmetrics[q].is_graph_ppsl[label] = true;
+            // }
+          }
+#ifndef BENCH
+          auto graph_stop = std::chrono::high_resolution_clock::system_clock::now();
+          auto graph_time = std::chrono::duration_cast<std::chrono::nanoseconds>(graph_stop - graph_start).count();
+          bm.qmetrics[q].graph_latency += graph_time;
+#endif
+          nround_graph++;
+        }
+        nround++;
+      }
+
+      bm.qmetrics[q].nround = nround;
+      bm.qmetrics[q].ncluster = clus_cnt;
+      bm.qmetrics[q].ncomp += this->graph_.GetNcomp(&state);
+      bm.qmetrics[q].ncomp_graph += this->graph_.GetNcomp(&state);
+      bm.qmetrics[q].ncomp_cg += this->cg_.GetNcomp(&cg_state);
+
+      graph_.Close(&state);
+      cg_.Close(&cg_state);
+      while (top_candidates.size() > k) top_candidates.pop();
+      results[q] = std::move(top_candidates);
+#ifndef BENCH
+      auto q_stop = std::chrono::high_resolution_clock::system_clock::now();
+      auto q_time = std::chrono::duration_cast<std::chrono::nanoseconds>(q_stop - q_start).count();
+      bm.qmetrics[q].latency = q_time;
+#endif
+    }
+    return results;
+  }
+
+  virtual vector<priority_queue<pair<dist_t, labeltype>>> SearchKnnPostFilteredTwoHopBit(
+      const void *query,
+      const int nq,
+      const int k,
+      const attr_t *attrs,
+      const attr_t *l_bound,
+      const attr_t *u_bound,
+      const int efs,
+      const int nrel,
+      const int nthread,
+      BatchMetric &bm
+  ) {
+    vector<priority_queue<pair<dist_t, labeltype>>> results(nq);
+
+    RangeQuery<attr_t> pred(l_bound, u_bound, attrs, this->n_, this->da_);
+    VisitedList *vl = this->graph_.hnsw_->visited_list_pool_->getFreeVisitedList();
+    VisitedList *vl_cg = this->cg_.hnsw_->visited_list_pool_->getFreeVisitedList();
+    roaring::Roaring bitmaps[4];
+    vector<uint32_t> ans;
+
+    // graph_.SetSearchParam(20, 20, k);
+    // cg_.SetSearchParam(20, 20, 20);
+
+    for (int q = 0; q < nq; q++) {
+#ifndef BENCH
+      auto q_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+      vl->reset();
+      priority_queue<pair<dist_t, labeltype>> top_candidates;
+      priority_queue<pair<dist_t, labeltype>> top_ivf;
+      const void *query_q = (char *)query + (q * graph_.hnsw_->data_size_);
+#ifndef BENCH
+      auto graph_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+      graph_.SetSearchParam(k, k, k);
+      // graph_.SetSearchParam(k, efs, k); // For testing non-iterative version.
+      auto state = graph_.OpenTwoHop(query_q, graph_.hnsw_->max_elements_, &pred, vl);
+      graph_.SetSearchParam(k / 2, k + k / 2, k / 2);
+#ifndef BENCH
+      auto graph_stop = std::chrono::high_resolution_clock::system_clock::now();
+      auto graph_time = std::chrono::duration_cast<std::chrono::nanoseconds>(graph_stop - graph_start).count();
+      bm.qmetrics[q].graph_latency += graph_time;
+#endif
+
+      decltype(ans.begin()) itr_beg, itr_end;
+      IterativeSearchState<dist_t> cg_state(query_q, k);
+      bool initialized = false;
+      int clus_cnt = 0;
+
+      int nround_graph = 0, num_graph_ppsl = 0, nround = 0;
+      int num_ivf_ppsl = 0;
+      int graph_last_round = 0;
+      double breaktie = 0.05;
+      while (top_candidates.size() < efs) {
+        // while (top_candidates.size() < k) { // For testing non-iterative version.
+        // IVF is responsible for negative clustering and extremely low passrate.
+        // Otherwise, post-filtering on graph should do.
+        if ((nround_graph >= 1 && (state.sel_ <= breaktie || graph_last_round == 0))) {
+#ifndef BENCH
+          auto ivf_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+          if (!initialized) {
+            vl_cg->reset();
+#ifndef BENCH
+            auto cg_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+            cg_state = cg_.Open(query_q, cg_.hnsw_->max_elements_, vl_cg);
+            auto next = cg_.Next(&cg_state);
+#ifndef BENCH
+            auto cg_stop = std::chrono::high_resolution_clock::system_clock::now();
+            auto cg_time = std::chrono::duration_cast<std::chrono::nanoseconds>(cg_stop - cg_start).count();
+            bm.qmetrics[q].cg_latency += cg_time;
+#endif
+            for (int i = 0; i < this->da_; i++) {
+              bitmaps[i].removeRange(0, this->n_);
+            }
+            int clus = next.second;
+            auto beg = mbtrees_[clus * this->da_ + 0].lower_bound(l_bound[0]);
+            auto end = mbtrees_[clus * this->da_ + 0].upper_bound(u_bound[0]);
+            while (beg != end) {
+              bitmaps[0].add(beg->second);
+              beg++;
+            }
+            for (int i = 1; i < this->da_; i++) {
+              auto beg = mbtrees_[clus * this->da_ + i].lower_bound(l_bound[i]);
+              auto end = mbtrees_[clus * this->da_ + i].upper_bound(u_bound[i]);
+              while (beg != end) {
+                bitmaps[i].add(beg->second);
+                beg++;
+              }
+              bitmaps[0] &= bitmaps[i];
+            }
+            ans.reserve(bitmaps[0].cardinality());
+            bitmaps[0].toUint32Array(ans.data());
+            itr_beg = ans.begin();
+            itr_end = ans.end();
+            initialized = true;
+            clus_cnt++;
+          }
+          int crel = 0;
+          while (crel < nrel) {
+            if (itr_beg == itr_end) {
+#ifndef BENCH
+              auto cg_start = std::chrono::high_resolution_clock::system_clock::now();
+#endif
+              auto next = cg_.Next(&cg_state);
+#ifndef BENCH
+              auto cg_stop = std::chrono::high_resolution_clock::system_clock::now();
+              auto cg_time = std::chrono::duration_cast<std::chrono::nanoseconds>(cg_stop - cg_start).count();
+              bm.qmetrics[q].cg_latency += cg_time;
+#endif
+              int clus = next.second;
+              if (clus == -1) {
+                break;
+              }
+              for (int i = 0; i < this->da_; i++) {
+                bitmaps[i].removeRange(0, this->n_);
+              }
+              auto beg = mbtrees_[clus * this->da_ + 0].lower_bound(l_bound[0]);
+              auto end = mbtrees_[clus * this->da_ + 0].upper_bound(u_bound[0]);
+              while (beg != end) {
+                bitmaps[0].add(beg->second);
+                beg++;
+              }
+              for (int i = 1; i < this->da_; i++) {
+                auto beg = mbtrees_[clus * this->da_ + i].lower_bound(l_bound[i]);
+                auto end = mbtrees_[clus * this->da_ + i].upper_bound(u_bound[i]);
+                while (beg != end) {
+                  bitmaps[i].add(beg->second);
+                  beg++;
+                }
+                bitmaps[0] &= bitmaps[i];
+              }
+              ans.reserve(bitmaps[0].cardinality());
+              bitmaps[0].toUint32Array(ans.data());
+              itr_beg = ans.begin();
+              itr_end = ans.end();
+              clus_cnt++;
+              continue;
+            }
+            tableint tableid = *itr_beg;
+            itr_beg++;
+#ifdef USE_SSE
+            if (itr_beg != itr_end) _mm_prefetch(this->graph_.hnsw_->getDataByInternalId(*itr_beg), _MM_HINT_T0);
 #endif
             if (vl->mass[tableid] == vl->curV) {
               continue;
